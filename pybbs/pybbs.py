9919468cfd94613222a23e79e5b02aea4105e053
Curtis F Kaylor <revcurtis@gmail.com>
2018-10-31 23:29:31 -0400

    Added files and mail to pybbs

--- pybbs/pybbs.py
+++ pybbs/pybbs.py
@@ -2,15 +2,16 @@
 '''Plain Text BBS'''
 
 import ConfigParser
+from datetime import datetime
+import os
 import re
 import select
 import socket
 import SocketServer
+import sqlite3
 import string
 import threading
 import time
-
-DEBUG = 8
 
 CONFIG_FILE = "pybbs.cfg"
 
@@ -18,8 +19,14 @@ DEFAULT_CONFIG = {
 'IPADDR': "127.0.0.1",
 'PORT': "2323", 
 'TIMEOUT': "0",
-'DEBUG': "8"
+'DEBUG': "4",
+'LOGFILE': None,
+'FILEDIR': None,
+'DBNAME': "pybbs.db"
 }
+
+DATE_FORMAT = '%Y-%m-%d'
+TIME_FORMAT = '%H:%M:%S'
 
 #Constants
 BBSNAME = "RetroBBS"
@@ -28,20 +35,29 @@ PROMPT = ">"
 QUITMSG = "Thank you for calling " + BBSNAME
 LOGOUT = "Logging Out"
 
-#Character Constants
-NUL = '\x00'  # ^@ - Null
-BRK = '\x03'  # ^C - Break
-BEL = '\x07'  # ^G - Bell
-BS  = '\x08'  # ^H - Backspace
-LF  = '\x0A'  # ^J - Line Feed
-VT  = '\x0B'  # ^K - Vertical Tab
-FF  = '\x0C'  # ^L - Form Feed
-CR  = '\x0D'  # ^M - Carriage Return
-ESC = '\x1B'  # ^[ - Escape
-SPC = ' '     #    - Space
-DEL = '\x7F'  #      Delete
+#ASCII Control Code Constants
+NUL  = '\x00' # ^@ - Null
+BRK  = '\x03' # ^C - Break
+BEL  = '\x07' # ^G - Bell
+BS   = '\x08' # ^H - Backspace
+HT   = '\x09' # ^I - Horizontal Tab
+LF   = '\x0A' # ^J - Line Feed
+VT   = '\x0B' # ^K - Vertical Tab
+FF   = '\x0C' # ^L - Form Feed
+CR   = '\x0D' # ^M - Carriage Return
+ESC  = '\x1B' # ^[ - Escape
+SPC  = ' '    #    - Space
+DEL  = '\x7F' #      Delete
+
+#Telnet Command Code Sequences
+ECHO = '\x01' #      Echo Option
+DONT = '\xFE' #      Don't Command
+IAC  = '\xFF' #      Interpret as Command
+
+#Character Sequence Constants
 CLRSCRN = SPC + FF + BS
 NEWLINE = CR + LF
+ECHOOFF = IAC + DONT + ECHO
 
 class Config(object):
   def __init__(self):
@@ -53,7 +69,6 @@ class Config(object):
     else:
       print "Unable to read file", CONFIG_FILE
   def getstr(self, section, option):
-    print self.config
     try: 
       result = self.config.get(section, option)
       print "Using config file section", section, "option", option
@@ -68,6 +83,68 @@ class Config(object):
       print ValueError, "in section", section, "option", option
       quit()
 
+class Database(object):
+  def __debug(self, message, level=0):
+    if self.debug: self.debug(message, level)
+  def __init__(self, dbname, debug=None):
+    self.debug = debug
+    self.__debug('Opening Database ' + dbname)
+    self.conn = sqlite3.connect(dbname)
+    self.crsr = self.conn.cursor()
+  def execute(self, query):
+    self.__debug('Executing Query: ' + query, 6)
+    self.crsr.execute(query)
+    self.conn.commit()
+  def create_table(self, table, columns):
+    self.__debug('Creating Database Table ' + table, 4)
+    query = 'CREATE TABLE IF NOT EXISTS ' + table + ' ( ' + columns +' )'
+    self.execute(query)
+  def read_rows(self, table, criteria):
+    self.__debug('Reading Rows from Table ' + table, 4)
+    self.__debug('With Criteria ' + str(criteria), 4)
+    where = []
+    what = []
+    for column, value in criteria.items():
+      where.append(column + '=?')
+      what.append(value)
+    query = 'SELECT * FROM ' + table + ' WHERE ' + ', '.join(where)
+    columns = tuple(what)
+    self.__debug('Query: ' + query, 4)
+    self.__debug('Columns: ' + str(columns), 4)
+    self.crsr.execute(query, columns)
+    return self.crsr.fetchall()
+  def write_row(self, table, columns):
+    self.__debug('Writing Row to Table ' + table, 4)
+    query = 'INSERT INTO ' + table + ' VALUES(?,?,?,?,?)'
+    self.__debug('Query: ' + query)
+    self.__debug('Columns: ' + str(columns))
+    self.crsr.execute(query, columns)
+    self.conn.commit()    
+
+class Log(object):
+  def __getthreadid(self):
+    thread = threading.current_thread()
+    threadid = thread.ident
+    if threadid < 0: threadid += 4294967296
+    return threadid
+  def __init__(self, threadid = None):
+    if threadid:
+      self.threadid = threadid
+    else:
+      self.threadid = self.__getthreadid()
+  def open(self, logname):
+    self.logname = logname
+    if self.logname: 
+      self.logfile = open(self.logname, "a")
+    else:
+      self.logfile = None
+  def write(self, entry):
+    if self.logfile:
+      logtime = datetime.now().strftime(DATE_FORMAT + ' ' + TIME_FORMAT)
+      line = "{0:} {1:}: {2:}\n".format(logtime, self.threadid, entry)
+      self.logfile.write(line)
+      self.logfile.flush()
+
 class BBS(object):
   def __clearInBuffer(self):
     self.inbuffer = ""
@@ -75,18 +152,21 @@ class BBS(object):
     if level <= self.debuglevel:
       decoded = message.encode('string_escape')
       print self.threadid, decoded
-  def __getthreadid(self):
-    thread = threading.current_thread()
-    threadid = thread.ident
-    if threadid < 0: threadid += 4294967296
-    return threadid
   def __config(self):
     config = Config()
     self.config = config
     self.debuglevel = config.getint('BBS', 'DEBUG')
     self.timeout = config.getint('BBS', 'TIMEOUT')
+    self.filedir = config.getstr('BBS', 'FILEDIR')
+    self.dbname = config.getstr('BBS', 'DBNAME')
+  def __getthreadid(self):
+    thread = threading.current_thread()
+    threadid = thread.ident
+    if threadid < 0: threadid += 4294967296
+    return threadid
   def __init__(self, socket):
     self.socket = socket
+    self.user_ip, self.user_port = self.socket.getpeername()
     self.echo = True
     self.threadid = self.__getthreadid()
     self.__config()
@@ -109,6 +189,10 @@ class BBS(object):
     self.write(data + NEWLINE)
   def writePrompt(self, data):
     self.write(NEWLINE + data + SPC)
+  def writeBlock(self, block):
+    self.writeLine()
+    for line in block:
+      self.writeLine(line)
   def fillbuffer(self):
     data = self.recv()
     if data:
@@ -140,11 +224,20 @@ class BBS(object):
       elif bs: line = line[:bs-1] + line[bs+1:]
       else: line = line[1:]
     self.__debug("read>"+line+nl, 8)
+    if nl == LF: self.write(CR+LF)
     return line
-  def readCommand(self, prompt='Command'):
-    command = self.readLine(prompt+">").strip().upper()
+  def readCommand(self, prompt='Command>'):
+    command = self.readLine(prompt).strip().upper()
     self.__debug("Received command "+command, 2)
     return command
+  def readBlock(self, desc='Text'):
+    block = []
+    self.writeLine('Enter ' + desc + ' below')
+    self.writeLine('Single . on a line to exit')
+    while True:
+      line = self.readLine()
+      if line.strip() == '.': return block
+      block.append(line)
   def elapsed(self, format=None):
     '''Returns time elapsed since start()'''
     _elapsed = int(time.time() - self.time_login)
@@ -165,28 +258,127 @@ class BBS(object):
         time.sleep(.5)
         self.writeLine("Invalid User Name")
     self.__debug("Logged in as "+self.username, 2)
+    self.log.write('User ' + self.username + ' logged in')
   def info(self):
     self.writeLine("Enter ? for Help")
   def main(self):
+    self.menu = self.main_menu
     while True:
       command = self.readCommand()
-      if command == 'HELP' or command == '?':
-        self.writeLine('HELP Display this text')
-        self.writeLine('USER Display user info')
-        self.writeLine('QUIT Log out of BBS')        
-      elif command == 'USER':
-        self.writeLine('User Name: '+self.username)
-        self.writeLine('Online for '+str(self.elapsed())+' seconds')
-      elif command == 'QUIT':
+      if command == 'QUIT' or command == 'Q':
         self.writeLine(QUITMSG)
         self.writeLine(LOGOUT)
+        self.log.write('User ' + self.username + ' logged out')
         break
       else:
+        self.menu(command)
+  def main_menu(self, command):
+      if command == 'HELP' or command == '?':
+        self.writeLine('HELP Display this text')
+        if self.filedir: self.writeLine('FILE Enter File Library')
+        self.writeLine('MAIL Electronic Mail')
+        self.writeLine('USER Display user info')
+        self.writeLine('QUIT Log out of BBS')        
+      elif command == 'USER' or command == 'U':
+        self.writeLine('User Name: '+self.username)
+        self.writeLine('IP Address: '+self.user_ip)
+        self.writeLine('Online for '+str(self.elapsed())+' seconds')
+      elif command == 'FILE' or command == 'F':
+        if self.filedir:
+          self.writeLine('Entering File Library')        
+          self.menu = self.file_menu
+        else: self.writeLine('File Library Not Available')
+      elif command == 'MAIL' or command == 'M':
+          self.writeLine('Electronic Mail Menu')        
+          self.menu = self.mail_menu
+      else:
         self.writeLine('Invalid command.')
+  def file_menu(self, command):
+      if command == 'HELP' or command == '?':
+        self.writeLine('HELP Display this text')
+        self.writeLine('LIST Display File List')
+        self.writeLine('READ Read File Contents')
+        self.writeLine('EXIT Return to Main Menu')
+      elif command == 'LIST' or command == 'L':
+        files = os.listdir(self.filedir)
+        if len(files):
+          self.writeLine('File           Date   Size')
+          for file in files:
+            spec = os.path.join(self.filedir, file)
+            stat = os.stat(spec)
+            date = datetime.fromtimestamp(stat.st_ctime).strftime(' %m/%d/%y ')
+            size = str(stat.st_size)
+            line = file.ljust(12) + date + size
+            self.writeLine(line)
+        else:
+          self.writeLine('No Files in Library')
+      elif command == 'READ' or command == 'R':
+        name = self.readCommand("File Name?")
+        spec = os.path.join(self.filedir, name)
+        try:
+          with open(spec) as file:
+            for line in file:
+              self.writeLine(line.strip())
+        except IOError as err:
+          self.writeLine("Error Opening File " + name)
+      elif command == 'EXIT' or command == 'X':
+        self.writeLine('Exiting File Library')        
+        self.menu = self.main_menu
+      else:
+        self.writeLine('Invalid command.')
+  def mail_menu(self, command):
+      if command == 'HELP' or command == '?':
+        self.writeLine('HELP Display this text')
+        self.writeLine('LIST Display Message List')
+        self.writeLine('READ Read Email Message')
+        self.writeLine('SEND Send Email Messages')
+        self.writeLine('EXIT Return to Main Menu')
+      elif command == 'LIST' or command == 'L':
+        criteria = {'RECIPIENT': self.username}
+        rows = self.db.read_rows('EMAIL', criteria)
+        for row in rows:
+          (sender, recipient, timestamp, subject, message) = row 
+          date_time = datetime.fromtimestamp(timestamp).strftime('%m/%d/%y %H:%M')
+          self.writeLine(date_time + ' ' + sender + ' - ' + subject)
+      elif command == 'READ' or command == 'R':
+        criteria = {'RECIPIENT': self.username}
+        rows = self.db.read_rows('EMAIL', criteria)
+        for row in rows:
+          (sender, recipient, timestamp, subject, message) = row 
+          date = datetime.fromtimestamp(timestamp).strftime('%m/%d/%y %H:%M')
+          block = message.split('\n')
+          self.writeLine('From: ' + sender)
+          self.writeLine('Date: ' + date)
+          self.writeLine('Subject: ' + subject)
+          self.writeBlock(block)
+          self.readLine('...Enter to Continue...')
+      elif command == 'SEND' or command == 'S':
+        sender = self.username
+        recipient = self.readLine('To User:')
+        if not recipient: return
+        subject = self.readLine('Subject')
+        if not subject: return
+        block = self.readBlock('Message')
+        message = '\n'.join(block)
+        timestamp = time.mktime(time.localtime())
+        columns = (sender, recipient, timestamp, subject, message)
+        self.db.write_row('EMAIL', columns)
+      elif command == 'EXIT' or command == 'X':
+        self.writeLine('Returning to Main Menu')        
+        self.menu = self.main_menu
+      else:
+        self.writeLine('Invalid command.')
+  def open_db(self):
+    self.db = Database(self.dbname, self.__debug)
+    self.db.create_table('EMAIL', 'SENDER TEXT, RECIPIENT TEXT, DATETIME REAL, SUBJECT TEXT, MESSAGE')
   def start(self):
+    self.log = Log(self.threadid)
+    self.log.open(self.config.getstr('BBS', 'LOGFILE'))
+    self.log.write('Connection from ' + self.user_ip + ' on Port ' + str(self.user_port))
     self.__debug("BBS Started", 1)
     self.time_login = time.time()
     self.enabletimeout()
+    self.open_db()
     try:
       self.welcome()
       self.login()
@@ -212,18 +404,22 @@ class BBS_Handler(SocketServer.BaseRequestHandler):
       
 class BBS_Server(SocketServer.TCPServer):
   allow_reuse_address = True
-  
+
 if __name__ == "__main__":
   config = Config()
   ipaddr = config.getstr('SERVER', 'IPADDR')
   port = config.getint('SERVER', 'PORT')
+  log = Log()
+  log.open(config.getstr('SERVER', 'LOGFILE'))
   server = BBS_Server((ipaddr, port), BBS_Handler)
   ipaddr, port = server.server_address
+  log.write('Server Opened on ' + ipaddr + ' Port ' + str(port))
   print "Listening on {0:} port {1:}".format(ipaddr, port)
   try:
     server.serve_forever()
   except KeyboardInterrupt:
     print "\nCaught Keyboard Interrupt"
+    log.write('Server terminated by keyboard interrupt')
   print 'Exiting Server'
   quit()
 
